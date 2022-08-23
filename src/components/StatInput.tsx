573839b66037cbb8b98ea144dd6cc16b0129fc20
BrandonXLF <brandfowl@gmail.com>
2022-08-23 02:42:50 -0400

    Use mathjs to parse input and in calculator

--- src/components/StatInput.tsx
+++ src/components/StatInput.tsx
@@ -2,14 +2,14 @@ import Stat, { StatTypes } from "../types/Stat";
 
 export default function StatInput(props: {
 	stat: Stat,
-	value: number,
+	value: string,
 	disabled: boolean,
-	onChange: (val: number) => void
+	onChange: (val: string) => void
 }) {
 	let percent = props.stat.type === StatTypes.Percent;
 	
 	return <>
 		<div className="mini-col">{percent ? '%' : ''}</div>
-		<input type="number" disabled={props.disabled} value={props.value} onChange={e => props.onChange(e.target.valueAsNumber)} />
+		<input type="text" title="Equation" disabled={props.disabled} value={props.value} onChange={e => props.onChange(e.target.value)} />
 	</>;
 }
