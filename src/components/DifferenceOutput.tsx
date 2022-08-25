78810860d76ac263b4de2dc7fd2b3cf23da041ab
BrandonXLF <brandfowl@gmail.com>
2022-08-25 11:41:15 -0400

    Don't show percent for NaN outputs

--- src/components/DifferenceOutput.tsx
+++ src/components/DifferenceOutput.tsx
@@ -2,10 +2,14 @@ import Popup from "reactjs-popup";
 
 export default function DifferenceOutput(props: {
 	initial?: number;
-	value: number
+	value?: number
 }) {
-	if (!props.initial) return null;
-	
+	if (
+		!props.initial || !props.value || Number.isNaN(props.initial) || Number.isNaN(props.value)
+	) {
+		return null;
+	}
+
 	let number = Math.round((props.value - props.initial) / props.initial * 100);
 	let className = number > 0 ? 'pos' : number < 0 ? ' neg' : '';
 	
