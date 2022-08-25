0c14ec892a47922ff7ce60cbff4d2f7338167f1a
BrandonXLF <brandfowl@gmail.com>
2022-08-25 12:56:56 -0400

    Don't show symbols for disabled StatInputs

--- src/components/StatInput.tsx
+++ src/components/StatInput.tsx
@@ -10,10 +10,10 @@ export default function StatInput(props: {
 	let percent = props.stat.type === StatTypes.Percent;
 	
 	return <FormInput
-		frontIcon={<div className="input-icon-math">
+		frontIcon={!props.disabled && <div className="input-icon-math">
 			<div>+</div><div>&minus;</div><div>&times;</div><div>&divide;</div>
 		</div>}
-		backIcon={percent && <div>%</div>}
+		backIcon={!props.disabled && percent && <div>%</div>}
 		frontIconLabel="Equation"
 		backIconLabel="Percent"
 	>
