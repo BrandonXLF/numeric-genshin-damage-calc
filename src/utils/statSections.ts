c156743dde59da58ed3269f75c80e7465ad7b3fd
BrandonXLF <brandfowl@gmail.com>
2022-12-11 04:15:47 -0500

    Added StatSection

---
+++ src/utils/statSections.ts
@@ -0,0 +1,14 @@
+import StatSection, { StatSections } from "../types/StatSection";
+
+const statSections: StatSection[] = [
+	{
+		name: 'Character',
+		value: StatSections.Character
+	},
+	{
+		name: 'Enemy',
+		value: StatSections.Enemy
+	}
+];
+
+export default statSections;
