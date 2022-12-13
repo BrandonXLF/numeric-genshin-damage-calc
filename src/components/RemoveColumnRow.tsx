b4f3f7fc52fe80cfd100c008789803d9134a02c6
BrandonXLF <brandfowl@gmail.com>
2022-12-13 17:36:12 -0500

    Cleaned up html and css

--- src/components/RemoveColumnRow.tsx
+++ src/components/RemoveColumnRow.tsx
@@ -11,43 +11,39 @@ export default function RemoveColumnRow(props: {
 }) {
 	return <>
 		{props.columns.map((inputDetails, i) =>
-			inputDetails.shown && <div key={i} style={{ display: 'flex', justifyContent: 'center', gap: '0.5em'}}>
-				<div>
-					<SVGButton
-						svg={<SaveSVG />}
-						label="Save and Close Column"
-						hideLabel={true}
-						onClick={() => {
-							let newColumns = [...props.columns];
-							
-							newColumns[i].shown = false;
-							
-							if (!newColumns.some(inputDetails => inputDetails.shown)) {
-								newColumns.push(createInputDetails());
-							}
-							
-							props.setColumns(newColumns);
-						}}
-					/>
-				</div>
-				<div style={{textAlign: 'center'}}>
-					<SVGButton
-						svg={<DeleteSVG className="neg" />}
-						label="Delete Column"
-						hideLabel={true}
-						onClick={() => {
-							let newColumns = [...props.columns];
-							
-							newColumns.splice(i, 1);
-							
-							if (!newColumns.some(inputDetails => inputDetails.shown)) {
-								newColumns.push(createInputDetails());
-							}
-							
-							props.setColumns(newColumns);
-						}}
-					/>
-				</div>
+			inputDetails.shown && <div key={i} className="column-top">
+				<SVGButton
+					svg={<SaveSVG />}
+					label="Save and Close Column"
+					hideLabel={true}
+					onClick={() => {
+						let newColumns = [...props.columns];
+						
+						newColumns[i].shown = false;
+						
+						if (!newColumns.some(inputDetails => inputDetails.shown)) {
+							newColumns.push(createInputDetails());
+						}
+						
+						props.setColumns(newColumns);
+					}}
+				/>
+				<SVGButton
+					svg={<DeleteSVG className="neg" />}
+					label="Delete Column"
+					hideLabel={true}
+					onClick={() => {
+						let newColumns = [...props.columns];
+						
+						newColumns.splice(i, 1);
+						
+						if (!newColumns.some(inputDetails => inputDetails.shown)) {
+							newColumns.push(createInputDetails());
+						}
+						
+						props.setColumns(newColumns);
+					}}
+				/>
 			</div>
 		)}
 	</>;
