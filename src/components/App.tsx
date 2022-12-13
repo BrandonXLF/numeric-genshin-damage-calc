b06995463ff48d3e9ccaaf67a31d807ac76975ad
BrandonXLF <brandfowl@gmail.com>
2022-12-13 04:44:36 -0500

    Adjusted visuals

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,13 @@
 import CalculatorForm from './CalculatorForm';
-
 import '../css/index.css';
 import 'reactjs-popup/dist/index.css';
 import '../css/popup.css';
+import { Footer } from './Footer';
 
 export default function App() {
 	return <main className='center-items'>
 		<h1>Numeric Genshin Damage Calculator</h1>
 		<CalculatorForm />
-		<footer>
-			<div>
-				Calculate in-game character damage for Genshin Impact based off of numeric stat values. 
-			</div>
-			<div>
-				<a href="https://github.com/BrandonXLF/numeric-genshin-damage-calc">Source code</a>
-			</div>
-		</footer>
+		<Footer />
 	</main>;
 }
