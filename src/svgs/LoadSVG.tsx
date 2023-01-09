987746c2a85ac3b40db55510c8dc2eaf081f74d2
BrandonXLF <brandfowl@gmail.com>
2023-01-09 01:50:45 -0500

    Updated SVGs

--- src/svgs/LoadSVG.tsx
+++ src/svgs/LoadSVG.tsx
@@ -1,8 +1,8 @@
 import { SVGProps } from "react";
 
 export default function LoadSVG(props: SVGProps<SVGSVGElement>) {
-	return <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" fill="none">
-		<path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2"></path>
+	return <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" fill="none" strokeLinecap="round">
+		<path d="M 4 17 v 2 a 2 2 0 0 0 2 2 h 12 a 2 2 0 0 0 2 -2 v -2"/>
 		<polyline points="7 9 12 4 17 9"></polyline>
 		<line x1="12" y1="4" x2="12" y2="16"></line>
 	</svg>;
--- src/svgs/SaveSVG.tsx
+++ src/svgs/SaveSVG.tsx
@@ -1,8 +1,8 @@
 import { SVGProps } from "react";
 
 export default function SaveSVG(props: SVGProps<SVGSVGElement>) {
-	return <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" fill="none">
-		<path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2"></path>
+	return <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" fill="none" strokeLinecap="round">
+		<path d="M 4 17 V 19 A 2 2 0 0 0 6 21 H 18 A 2 2 0 0 0 20 19 V 17"/>
 		<polyline points="7 11 12 16 17 11"></polyline>
 		<line x1="12" y1="4" x2="12" y2="16"></line>
 	</svg>;
