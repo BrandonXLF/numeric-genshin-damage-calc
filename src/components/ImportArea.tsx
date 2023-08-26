02abead98b257d5593ba3a3e1fbed151888bb0bc
Brandon Fowler <brandfowl@gmail.com>
2023-08-26 04:42:41 -0400

    Make SVGButton mini

--- src/components/ImportArea.tsx
+++ src/components/ImportArea.tsx
@@ -105,6 +105,7 @@ export default function ImportArea(props: {
                     svg={<SearchSVG />}
                     label="Search"
                     onClick={() => setUid(inProgressUID)}
+                    mini
                 />
             </div>
         </div>
