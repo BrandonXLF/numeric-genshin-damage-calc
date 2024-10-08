6e04a99cfdb0c33b9f32485ed32a051030a26e0d
Brandon Fowler <brandfowl@gmail.com>
2024-10-07 21:07:19 -0400

    Fix build for now test will need to be fixed later

--- src/utils/csv.ts
+++ src/utils/csv.ts
@@ -1,4 +1,4 @@
-import { stringify } from "csv-stringify/dist/esm/sync";
+import { stringify } from "csv-stringify/browser/esm/sync";
 import Attack from "./Attack";
 import Stat, { StatType } from "../types/Stat";
 import Column from "./Column";
@@ -6,7 +6,7 @@ import DamageCalculator from "./DamageCalculator";
 import damageTypes from "./damageTypes";
 import displayDamage from "./displayDamage";
 import stats from "./stats";
-import { parse } from "csv-parse/dist/esm/sync";
+import { parse } from "csv-parse/browser/esm/sync";
 import attributes, { getAttrStat } from "./attributes";
 import statSections from "./statSections";
 import { StoredAttack } from "../types/PartialAttack";
