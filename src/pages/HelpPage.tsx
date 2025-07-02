1f8cb192325650037a8488480265f8cf8446fb3f
Brandon Fowler <brandfowl@gmail.com>
2025-07-02 14:58:23 -0400

    Add a table of contents to the help page

--- src/pages/HelpPage.tsx
+++ src/pages/HelpPage.tsx
@@ -7,30 +7,50 @@ import { Fragment } from "react/jsx-runtime";
 import { topDescs } from "../utils/TopDescs";
 import '../less/HelpPage.less';
 import Page from "../components/Page";
+import damageTypes from "../utils/damageTypes";
+
+const entries = [
+	{
+		name: 'Column Options',
+		section: 'general',
+		data: [...topDescs.entries()].map(([name, desc]) => ({ name, desc }))
+	},
+	{
+		name: 'Stat Information',
+		section: 'stats',
+		data: stats
+	},
+	{
+		name: 'Damage Types',
+		section: 'damage',
+		data: damageTypes
+	}
+];
 
 export default function HelpPage() {
 	return <Page title="Form Information and Help">
 		<div className="help-page">
 			<h2>Form Information and Help</h2>
 			<Link to="/">&larr; Back to Calculator</Link>
-			<h3>Column Options</h3>
-			<dl>
-				{[...topDescs.entries()].map(([name, desc]) =>
-					<Fragment key={name}>
-						<dt>{name}</dt>
-						<dd>{desc ?? '-'}</dd>
-					</Fragment>
-				)}
-			</dl>
-			<h3>Stat Information</h3>
-			<dl>
-				{stats.map(stat =>
-					<Fragment key={stat.name}>
-						<dt>{stat.name}</dt>
-						<dd>{stat.desc ?? '-'}</dd>
-					</Fragment>
-				)}
-			</dl>
+			<nav aria-labelledby="toc-label">
+				<span id="toc-label">Sections:</span>{''}
+				<ol className="toc">
+					{entries.map(entry => <li key={entry.name}>
+						<a href={`#${entry.section}`}>{entry.name}</a>
+					</li>)}
+				</ol>
+			</nav>
+			{entries.map(entry => <Fragment key={entry.name}>
+				<h3 id={entry.section}>{entry.name}</h3>
+				<dl aria-labelledby={entry.section}>
+					{entry.data.map(({ name, desc }) =>
+						<Fragment key={name}>
+							<dt>{name}</dt>
+							<dd>{desc ?? '-'}</dd>
+						</Fragment>
+					)}
+				</dl>
+			</Fragment>)}
 		</div>
 	</Page>;
 }
