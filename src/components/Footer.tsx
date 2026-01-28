dd9fb196e7c8c9993b689a26e9e2114a95575031
Brandon Fowler <brandfowl@gmail.com>
2026-01-27 20:05:39 -0500

    Add link to other tools

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,8 @@ export function Footer() {
 			Calculate in-game character damage for Genshin Impact based off of numeric stat values. 
 		</div>
 		<div>
-			<a href="https://github.com/BrandonXLF/numeric-genshin-damage-calc">Source code</a>
+			Developed by Brandon Fowler (<a href="https://www.brandonfowler.me/genshin-tools/">other tools</a>).
+			{' '}<a href="https://github.com/BrandonXLF/numeric-genshin-damage-calc">View source code</a>.
 		</div>
 	</footer>;
 }
